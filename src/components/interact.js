656b2f8951160bac78b5c5c96c4c61a369ef60b7
Daniel Graf <grafmark59@gmail.com>
2017-11-02 10:17:20 -0400

    primative modal works

--- src/components/interact.js
+++ src/components/interact.js
@@ -1,11 +1,42 @@
-import React from "react"
+import React, { Component } from "react"
+import Modal from './modal';
 
-export default ({ children }) =>
-  <div className='interact'
-    style={
-      {
-        // paddingBottom: '12px'
-      }
-    }>
-    {children}
-  </div>
+class Interact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isOpen: false };
+  }
+
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='interact' onClick={this.toggleModal}>
+          { this.props.children }
+        </div>
+        <Modal show={this.state.isOpen}
+          onClose={this.toggleModal}>
+          Here's some content for the modal
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default Interact;
+
+// export default ({ children }) =>
+//   <div className='interact'
+//     style={
+//       {
+//         // paddingBottom: '12px'
+//       }
+//     }>
+//     {children}
+//   </div>
